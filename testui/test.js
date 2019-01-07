070c8d27fefb39a9bf4ead41aedb504028d879f2
Eloim <mouse.87rus@gmail.com>
2019-01-07 17:31:57 +0300

    init tests

---
+++ testui/test.js
@@ -0,0 +1,29 @@
+const webdriverio = require('webdriverio');
+const t = require('tap');
+const {sel} = require('../utils/global');
+
+const options = {
+	desiredCapabilities: {
+		browserName: 'chrome'
+	},
+	deprecationWarnings: false
+};
+
+
+t.test('TestLogin', async (t) => {
+	const driver = webdriverio.remote(options);
+	await driver.init();
+
+	await t.test('check title', async (t) => {
+		await driver.url('http://127.0.0.1:3000/');
+		// await driver
+		// 	.setValue(sel('sidebar-input-username'), 'jijka')
+		// 	.setValue(sel('sidebar-input-password'), '123123')
+		// 	.click(sel('sidebar-btn-submit'))
+		// 	.waitForExist(sel('sidebar-username'), 10000);
+
+		// const username = await driver.getText(sel('sidebar-username'));
+		// t.contains(username, 'jijka');
+	});
+	// await driver.end();
+});
